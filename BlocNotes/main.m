8e466bb2e6a777fa6b3da2b257117584cc10e055
Thomas Steimann <thomas.steimann@resolvex.ch>
2015-02-27 12:06:25 +0100

    Initial Commit

---
+++ BlocNotes/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BlocNotes
+//
+//  Created by tom on 27/02/15.
+//  Copyright (c) 2015 Resolvex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
